0afda1e20ab4884eebca3d8ddb8d394b990a9c94
Giap Nguyen Huu <giapnguyen74@gmail.com>
2017-10-25 15:02:40 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const gstate = (query, renderer, options = {}) => {
+	class Renderer extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				data: undefined
+			};
+
+			if (!props.state) {
+				throw new Error(
+					"Could not find state in props of gstate component. Please explicitly pass state as a prop to gstate component."
+				);
+			}
+		}
+
+		refetch() {
+			this._watcher && this._watcher();
+			this._watcher = this.props.state.watch(query, data => {
+				this.setState({
+					data: data
+				});
+			});
+		}
+
+		componentDidMount() {
+			options.mounted && options.mounted.call(this);
+			this.refetch();
+		}
+
+		componentWillUnmount() {
+			this._watcher && this._watcher();
+		}
+
+		render() {
+			return renderer(this.props, this.state.data, this);
+		}
+	}
+	return Renderer;
+};
+
+export default gstate;
